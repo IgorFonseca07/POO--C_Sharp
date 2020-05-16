15492c4a3067358586b6c85bfed24a31e5b61931
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-15 22:55:21 -0300

    Create MemEst

---
+++ 07-Exerc07MemEst/Exerc07MemEst/ConversorDeMoeda.cs
@@ -0,0 +1,9 @@
+﻿namespace Exerc07MemEst {
+    class ConversorDeMoeda {
+
+        public static double PagarEmReais(double cot, double com) {            
+            return (com * cot) + (cot * (com * 0.06));
+        }
+
+    }
+}
---
+++ 07-Exerc07MemEst/Exerc07MemEst/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc07MemEst {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotdolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double comdolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pagreais = ConversorDeMoeda.PagarEmReais(cotdolar, comdolar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + pagreais.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
