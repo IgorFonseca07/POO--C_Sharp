c40749ed56399153ab2c89dc2635e350cae92ec8
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-17 18:44:01 -0300

    Constr Sobrec This

---
+++ 10-This/This/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace This {
+    class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        /*Esse abaixo é o primeiro exemplo de this, porém não utilizado em C# pois como a regra é: 
+         Atributo sempre iniciando com letra maiúscula e variável com letra minúscula, então não há chances de se 
+         confundirem. */
+
+        public Produto(string Nome) {
+            this.Nome = Nome;                        
+        }
+
+        public Produto() { /* Nesse caso aqui, se você mandasse printar na tela, nome seria null, double 0.0 e int 0 */
+            Quantidade = 10;
+        }
+
+        public Produto(string nome, double preco) : this() {
+            Nome = nome;
+            Preco = preco;
+            /* Quantidade = 0; <- poderia ou não ter declarado, pois o C# já entenderia que valeria 0 */
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
+            Quantidade = quantidade;
+        }
+
+        /*public Produto() {  /*Nesse caso aqui, se você mandasse printar na tela, nome seria null, double 0.0 e int 0
+        } */
+
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+        public override string ToString() {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ 10-This/This/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace This {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nom = Console.ReadLine();
+            Console.Write("Preço: ");
+            double prec = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nom, prec);
+
+            Produto x = new Produto(nom, prec, 15);
+
+            Produto y = new Produto();
+
+
+            /*Sintaxe alternativa para inicializar valores: */
+            Produto p2 = new Produto {
+                Nome = "TV",
+                Preco = 500.00,
+                Quantidade = 20
+            };
+            /*Não precisa ter construtores para esse último exemplo! */
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+        }
+    }
+}
