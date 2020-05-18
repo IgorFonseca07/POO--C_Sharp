c2d5b988f51802d1b9693f7a90d9ea04d1503a15
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-17 23:31:38 -0300

    Encaps

---
+++ 11-Encaps/Encaps/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encaps {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("T");
+            p.RemoverProdutos(5);
+            p.AdicionarProdutos(3);
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+
+        }
+    }
+}
