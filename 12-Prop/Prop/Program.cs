7028bf9025123384772ac512d79d3cc25009e394
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-20 20:25:05 -0300

    Prop

---
+++ 12-Prop/Prop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Prop {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+            p.RemoverProdutos(5);
+            p.AdicionarProdutos(3);
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+
+        }
+    }
+}
