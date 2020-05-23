75c6b0848c0cb6c3d99fdb8312557fc5fd109232
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-23 00:59:29 -0300

    Exerc08

---
+++ Exerc08/Exerc08/Conta.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Exerc08 {
+    class Conta {
+
+        public int NumeroConta { get; private set; }
+        public string TitularConta { get; private set; }
+        public double ValorSaldoAtual { get; private set; }
+
+
+        public Conta(int numeroconta, string titularconta) {
+            NumeroConta = numeroconta;
+            TitularConta = titularconta;
+        }
+
+        public Conta(int numeroconta, string titularconta, double valordepositoinicial) : this(numeroconta, titularconta) {
+            ValorSaldoAtual = valordepositoinicial;
+        }
+
+
+        public void Deposito(double deposito) {
+            ValorSaldoAtual += deposito;
+        }
+
+        public void Saque(double saque) {
+            ValorSaldoAtual = (ValorSaldoAtual - saque) - 5.00;
+        }
+
+        public override string ToString() {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + TitularConta
+                + ", Saldo: $ "
+                + ValorSaldoAtual.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Exerc08/Exerc08/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc08 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Entre com o número da conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titularconta = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (S/N) ");
+            string depositoinicial = Console.ReadLine();
+            double movimentacao = 0.00;
+
+            if (depositoinicial == "S") {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                movimentacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }                        
+
+            Conta n1 = new Conta(numeroconta, titularconta, movimentacao);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta:");
+            Console.WriteLine(n1);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para depósito: ");
+            movimentacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n1.Deposito(movimentacao);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(n1);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para saque: ");
+            movimentacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n1.Saque(movimentacao);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(n1);
+
+
+
+
+        }
+    }
+}
