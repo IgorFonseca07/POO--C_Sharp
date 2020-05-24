e891b0b3281856683ceaddc2c550ac8d0827abc8
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-24 12:43:06 -0300

    TiposRefer_TiposValor

---
+++ 15-TiposRefer_TiposValor/TiposRefer_TiposValor/Point.cs
@@ -0,0 +1,16 @@
+﻿namespace TiposRefer_TiposValor {
+    struct Point {
+
+        public double X;
+        public double Y;
+
+        public Point(double x, double y) {
+            X = x;
+            Y = y;
+        }
+        public override string ToString() {
+            return "( " + X + ", " + Y + " )";
+        }
+
+    }
+}
---
+++ 15-TiposRefer_TiposValor/TiposRefer_TiposValor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TiposRefer_TiposValor {
+    class Program {
+        static void Main(string[] args) {
+
+            Point p;
+
+            p.X = 20;
+            p.Y = 10;
+
+            Console.WriteLine(p);
+
+
+            // ou pode-se inicializar assim:
+
+            Point m = new Point();
+
+            Console.WriteLine(m);
+
+
+            // Caso houver um construtor:
+
+            Point n = new Point(40, 50);
+
+            Console.WriteLine(n);
+
+        }
+    }
+}
