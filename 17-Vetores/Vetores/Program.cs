f033f8a3104f4eb28c3008476f6323c93e347dae
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-24 18:38:51 -0300

    Vetores

---
+++ 17-Vetores/Vetores/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores {
+    class Program {
+        static void Main(string[] args) {
+
+
+            // Vetor eu já sabia do Javascript
+
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+            double sum = 0.00;
+            
+
+            for (int i = 0; i < n; i++) {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                sum += vect[i];
+            }
+
+            Console.WriteLine(sum);
+
+            double media = sum / (vect.Length);
+
+            Console.WriteLine(media);
+
+
+            Console.WriteLine();
+            //----------------------------------------------------------------------------------
+            Console.WriteLine();
+
+
+            // Exercício exemplo:
+
+            int numeroprodutos = int.Parse(Console.ReadLine());
+
+            Product[] vetor = new Product[n];
+            double soma = 0.00;
+
+            for (int i = 0; i < numeroprodutos; i++) {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vetor[i] = new Product { Nome = name, Preco = price };
+                soma += vetor[i].Preco;
+            }
+
+            double average = soma / (vetor.Length);
+
+            Console.WriteLine("AVERAGE PRICE = " + average.ToString("F2", CultureInfo.InvariantCulture));
+
+            
+
+        }
+    }
+}
