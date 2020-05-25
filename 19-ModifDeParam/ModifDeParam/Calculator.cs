46763342349512a6be67efb6579f785fbfd3d511
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-25 20:36:02 -0300

    ModifDeParam

---
+++ 19-ModifDeParam/ModifDeParam/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ModifDeParam {
+    class Calculator {
+
+        public int Soma(int n1, int n2) {
+            return n1 + n2;
+        }
+
+        public int Soma(int n1, int n2, int n3) {
+            return n1 + n2 + n3;
+        }
+
+        public int Soma(int n1, int n2, int n3, int n4) {
+            return n1 + n2 + n3 + n4;
+        }
+
+        //Fazer o procedimento acima é super inviável, teria que colocar infinitos métodos
+
+
+        //Modificador de Parâmetro Params:
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+
+        //Modificador de Parâmetro Ref:
+        public static void Triple(ref int x) {
+            x = x * 3;
+        }
+        
+
+        //Modificador de Parâmetro Out:
+        public static void Triplo(int x, out int y) {
+            y = x * 3;
+        }
+
+        
+
+    }
+}
---
+++ 19-ModifDeParam/ModifDeParam/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ModifDeParam {
+    class Program {
+        static void Main(string[] args) {
+
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 3, 4);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+
+            //-------------------------------------
+
+
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+
+
+            //-------------------------------------
+
+
+            int b = 15;
+            int c;
+            Calculator.Triplo(b, out c);
+            Console.WriteLine(c);
+
+            //OBS.: Usar Ref e Out é considerado "code smells" (design ruim) e devem ser evitados
+
+
+        }
+    }
+}
