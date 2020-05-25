0bb1a5b13e17ae14233d213db61059003b9aa0b5
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-24 21:32:51 -0300

    Exerc09

---
+++ 18-Exerc09/Exerc09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exerc09 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("How many rooms will be rented? ");
+            int roomsrented = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Rooms[] vect = new Rooms[10];
+
+            for (int i = 1; i <= roomsrented; i++) {
+                Console.WriteLine("Rent #" + i + ":");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Rooms { Name = name, Email = email };
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i <= 9; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }               
+            }
+
+
+        }
+    }
+}
---
+++ 18-Exerc09/Exerc09/Rooms.cs
@@ -0,0 +1,15 @@
+﻿namespace Exerc09 {
+    class Rooms {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+        public override string ToString() {
+            return Name
+                + ", "
+                + Email;               
+        }
+
+    }
+}
