8da83d97ed5aebe601d91e304df3393b05672f41
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-25 20:48:04 -0300

    BoxingUnboxing

---
+++ 20-BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BoxingUnboxing {
+    class Program {
+        static void Main(string[] args) {
+
+            //Boxing: Processo de conversão objeto tipo Valor para objeto tipo Referência compatível:
+
+            int x = 20;
+            Object obj = x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(obj);
+
+
+            //Unboxing: Processo de conversão objeto tipo Referência para objeto tipo Valor compatível:
+
+            int y = (int)obj;
+
+            Console.WriteLine(y);
+
+
+        }
+    }
+}
