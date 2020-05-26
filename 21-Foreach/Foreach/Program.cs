034fa0d100efb875ce104842a4199a4e5511dfd2
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-05-25 21:01:17 -0300

    Foreach

---
+++ 21-Foreach/Foreach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Foreach {
+    class Program {
+        static void Main(string[] args) {
+
+
+            //Muitas vezes usado mais que o For:
+
+
+            string[] names = new string[] { "Maria", "Alex", "Bob" };
+
+            for (int i = 0; i < names.Length; i++) {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.WriteLine("------------------------------------------");
+
+            foreach (string thenames in names) {
+                Console.WriteLine(thenames);
+            }
+        }
+    }
+}
