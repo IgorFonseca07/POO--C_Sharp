aff1d2f2174ec70ca83d3d4ea246e253efc34975
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-06 22:38:29 -0300

    Matrizes

---
+++ Exerc11/Exerc11/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exerc11 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantas linhas a matriz terá? ");
+            int m = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
