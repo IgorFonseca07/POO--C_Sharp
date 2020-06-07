b8dca3001d5dea8d2460f22a4741dee5f7b54805
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-06 22:28:04 -0300

    Matrizes

---
+++ 24-Matrizes/Matrizes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+
+            double[,] ex = new double[2, 3];
+
+            Console.WriteLine(ex.Length);
+
+            Console.WriteLine(ex.Rank);
+
+            Console.WriteLine(ex.GetLength(0));
+
+            Console.WriteLine(ex.GetLength(1));
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine();
+
+
+            Console.Write("Entre com o valor N da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrixn = new int[n, n];
+            int[] diagonal = new int[n];
+            int sumnegatives = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    Console.Write("Entre com o valor da linha " + i + ", coluna " + j + ": ");
+                    matrixn[i, j] = int.Parse(Console.ReadLine());
+                    if (i == j) {
+                        diagonal[i] = matrixn[i, j];
+                    }
+                    if (matrixn[i, j] < 0) {
+                        sumnegatives += 1;
+                    }
+                }
+            }
+
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++) {
+                Console.Write(diagonal[i] + "  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Negative Numbers = " + sumnegatives);
+
+
+
+        }
+    }
+}
