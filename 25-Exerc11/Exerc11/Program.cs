eb256417a892725170577b460bd5165007155fb2
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-07 15:23:13 -0300

    Exerc11

--- 25-Exerc11/Exerc11/Program.cs
+++ 25-Exerc11/Exerc11/Program.cs
@@ -22,12 +22,25 @@ namespace Exerc11 {
 
             Console.Write("What's the number X? ");
             int numberx = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     if (matrixmn[i, j] == numberx) {
                         Console.WriteLine("Position " + i + "," + j + ":");
-                        
+                        if (j > 0) {
+                            Console.WriteLine("Left: " + matrixmn[i, (j - 1)]);
+                        }
+                        if (i > 0) {
+                            Console.WriteLine("Up: " + matrixmn[(i - 1), j]);
+                        }
+                        if (j < (n - 1)) {
+                            Console.WriteLine("Right: " + matrixmn[i, (j + 1)]);
+                        }
+                        if (i < (m - 1)) {
+                            Console.WriteLine("Down: " + matrixmn[(i + 1), j]);
+                        }
+                        Console.WriteLine();
                     }
                 }
             }
