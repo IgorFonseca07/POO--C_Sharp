2f8d46ab918ee738ccf88601075a164d6b284b61
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-16 22:19:37 -0300

    Exerc13

---
+++ 31-Exerc13/Exerc13/Entities/Order.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Exerc13.Entities.Enums;
+
+namespace Exerc13.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus orderStatus, Client client)
+        {
+            Moment = moment;
+            OrderStatus = orderStatus;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            OrderItems.Add(orderItem);
+        }
+
+        public void RemoveItem(OrderItem orderItem)
+        {
+            OrderItems.Remove(orderItem);
+        }
+
+        public double Total()
+        {
+            double sum = 0.00;
+            foreach (OrderItem orderItem in OrderItems)
+            {
+                sum += orderItem.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY:");
+            sb.AppendLine("Order moment: " + Moment);
+            sb.AppendLine("Order status: " + OrderStatus);
+            sb.AppendLine("Client: " + Client.Name + " (" + Client.BirthDate.ToShortDateString() + ") - " + Client.Email);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem orderItem in OrderItems)
+            {
+                sb.Append(orderItem.Product.Name + ", $" + orderItem.Price.ToString("F2", CultureInfo.InvariantCulture));
+                sb.AppendLine(", Quantity: " + orderItem.Quantity + ", Subtotal: $" + orderItem.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ 31-Exerc13/Exerc13/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using Exerc13.Entities;
+using Exerc13.Entities.Enums;
+
+namespace Exerc13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data:");
+
+            Console.Write("Status: ");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int quantityItems = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Order order = new Order(DateTime.Now, orderStatus, client);
+
+            for (int i = 1; i <= quantityItems; i++)
+            {
+                Console.WriteLine("Enter #" + i + " item data:");
+
+                Console.Write("Product name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double priceProduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product product = new Product(nameProduct, priceProduct);
+
+                Console.Write("Quantity: ");
+                int quantityProduct = int.Parse(Console.ReadLine());
+                OrderItem orderItem = new OrderItem(quantityProduct, priceProduct, product);
+
+                order.AddItem(orderItem);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(order);
+
+
+        }
+    }
+}
