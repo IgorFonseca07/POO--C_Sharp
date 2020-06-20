7b560a0d99026d149003fbc77981b5dc6fb57531
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-19 22:24:14 -0300

    Sobreposicao_Virtual_Override_Base

---
+++ 34-Sobreposicao_Virtual_Override_Base/Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿namespace Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        //public override void Withdraw(double amount)       *Nesse caso aqui, você aproveitaria o método Withdraw da
+        //{                                                  *superclasse e descontaria mais 2 reais
+        //    base.Withdraw(amount);
+        //    Balance -= 2.0;    
+        //}
+    }
+}
