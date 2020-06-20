b09541feae0ff95baba144b45a84f1f64c375af0
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-19 22:39:52 -0300

    ClassesMetodosSelados

---
+++ 35-ClassesMetodosSelados/Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿namespace Heranca.Entities
+{
+    sealed class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount)     // Método selado! sealed
+        {
+            Balance -= amount;
+        }
+
+        //public override void Withdraw(double amount)       *Nesse caso aqui, você aproveitaria o método Withdraw da
+        //{                                                  *superclasse e descontaria mais 2 reais
+        //    base.Withdraw(amount);
+        //    Balance -= 2.0;    
+        //}
+    }
+}
---
+++ 35-ClassesMetodosSelados/Heranca/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca.Entities
+{
+    class SavingsAccountPlus : SavingsAccount
+    {
+        // Como selei a classe SavingsAccount, nenhuma classe pode herdar ela
+
+        public override void Withdraw(double amount)    // Como já foi selado o primeiro override, não se pode
+        {                                               //  mais sobrepor em nenhuma subclasse.
+
+        }
+    }
+}
