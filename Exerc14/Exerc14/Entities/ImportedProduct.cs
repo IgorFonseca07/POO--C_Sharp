0920709e58f33e3e6ce1346ea98bfa2d4805ed00
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-21 02:18:21 -0300

    Exerc14

---
+++ Exerc14/Exerc14/Entities/ImportedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Exerc14.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFree { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFree) : base(name, price)
+        {
+            CustomsFree = customsFree;
+        }
+
+        public double TotalPrice()
+        {
+            return CustomsFree + Price;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ "
+                + CustomsFree.ToString("F2",CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
---
+++ Exerc14/Exerc14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exerc14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
