cdb164267fea9d20af5ced8b53d9eb3eb01fe8ff
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-27 22:48:05 -0300

    ExcecoesPersonalizadas

---
+++ 42-ExcecoesPersonalizadas/ExcecoesPersonalizadas/Entities/Reservation.cs
@@ -0,0 +1,62 @@
+﻿using ExcecoesPersonalizadas.Entities.Exceptions;
+using System;
+
+namespace ExcecoesPersonalizadas.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date!");
+            }
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            if (checkin < DateTime.Now || checkout < DateTime.Now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates!");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date!");
+            }
+            
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + Checkin.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + Checkout.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+
+    }
+}
---
+++ 42-ExcecoesPersonalizadas/ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ExcecoesPersonalizadas.Entities;
+using ExcecoesPersonalizadas.Entities.Exceptions;
+
+namespace ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+    }
+}
