4b8c6b0a7b35a7059eec0b7c23b77ae7f387c36d
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-07-27 22:35:08 -0300

    FileStream StreamReader

--- 44-File_FileInfo_IOException_FileStream_StreamReader/44-File_FileInfo_IOException/Program.cs
+++ 44-File_FileInfo_IOException_FileStream_StreamReader/44-File_FileInfo_IOException/Program.cs
@@ -25,6 +25,37 @@ namespace _44_File_FileInfo_IOException
                 Console.WriteLine("An error occurred!");
                 Console.WriteLine(e.Message);
             }
+            Console.WriteLine();
+
+
+
+            // FileStream and StreamReader
+
+
+            // FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                // fs = new FileStream(sourcePath, FileMode.Open);
+                // sr = new StreamReader(fs);
+                sr = File.OpenText(sourcePath);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                //  if (fs != null) fs.Close();
+            }
 
         }
     }
