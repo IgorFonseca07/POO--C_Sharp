7ce0cdedb5b19ed6fa0e24f7848cc882424b6700
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-08-03 19:04:59 -0300

    Exerc18

---
+++ 48-Exerc18/48-Exerc18/Methods.cs
@@ -0,0 +1,12 @@
+﻿namespace _48_Exerc18
+{
+    class Methods
+    {
+
+        public double TotalPrice(double unitPrice, double quantity)
+        {
+            return (unitPrice * quantity);
+        }
+
+    }
+}
---
+++ 48-Exerc18/48-Exerc18/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace _48_Exerc18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string sourcePath = @"C:\Users\Caveira\Documents\CursoC#_Udemy\POO--C_Sharp\48-Exerc18\CSV";
+
+                Directory.CreateDirectory(sourcePath + @"\Out");
+
+                string targetPath = sourcePath + @"\Out";
+
+                string[] lines = File.ReadAllLines(sourcePath + @"\Products.csv");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                Console.WriteLine();
+
+                foreach (string line in lines)
+                {
+                    string[] columns = line.Split(';');
+
+                    double unitPrice = double.Parse(columns[1], CultureInfo.InvariantCulture);
+                    double quantity = double.Parse(columns[2], CultureInfo.InvariantCulture);
+
+                    using (StreamWriter sw = File.AppendText(targetPath + @"\Summary.csv"))
+                    {
+                        sw.WriteLine(columns[0] + ";" + new Methods().TotalPrice(unitPrice, quantity).ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+
+                lines = File.ReadAllLines(targetPath + @"\Summary.csv");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+        }
+    }
+}
