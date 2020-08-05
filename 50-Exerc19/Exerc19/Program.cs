2ddfadd09d98fbdee81ce787b60ddcc1b2ee17b0
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-08-05 16:23:19 -0300

    Exerc19

--- 50-Exerc19/Exerc19/Program.cs
+++ 50-Exerc19/Exerc19/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Exerc19.Entities;
+using Exerc19.Services;
+using System;
+using System.Globalization;
 
 namespace Exerc19
 {
@@ -6,7 +9,31 @@ namespace Exerc19
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
             
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Enter number of installments: ");
+            int numberOfInstallments = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(number, date, contractValue);
+            PaymentService paymentService = new PaymentService(numberOfInstallments, contract, new PaypalService());
+
+            paymentService.Payment(paymentService);
+            
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in paymentService.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+
         }
     }
 }
