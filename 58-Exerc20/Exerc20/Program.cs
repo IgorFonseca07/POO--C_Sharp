d17e60c70bd741143e23a20e98be89cb69c1da57
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-08-10 17:31:53 -0300

    Exerc20

---
+++ 58-Exerc20/Exerc20/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exerc20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            HashSet<string> names = new HashSet<string>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+
+                        string[] lines = line.Split(' ');
+                        names.Add(lines[0]);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred:" + e.Message);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Total users: " + names.Count);
+        }
+    }
+}
